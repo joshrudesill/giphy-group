6f79c5812329375a8d0428a79b820309ef119c57
Josh Rudesill <joshrudesill@gmail.com>
2024-02-29 10:26:01 -0600

    saga setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 function App() {
+  const dispatch = useDispatch();
+  const favs = useSelector((state) => state.favorites);
+  useEffect(() => {
+    dispatch({ type: "FETCH_FAV" });
+  }, []);
   return (
     <div>
       <h1>Giphy Search!</h1>
+      {JSON.stringify(favs)}
     </div>
   );
 }
-
 
 export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -8,23 +8,48 @@ function* test() {
   console.log("Hello from test saga");
 }
 
-function* rootSaga() {
-  yield takeLatest("TEST", test);
-}
-
 const sagaMiddleware = createSagaMiddleware();
 
 const favorites = (state = [], action) => {
+  switch (action.type) {
+    case "REFRESH_FAVORITES":
+      return action.payload;
+  }
   return state;
 };
 const categories = (state = [], action) => {
+  switch (action.type) {
+    case "REFRESH_CATS":
+      return action.payload;
+  }
   return state;
 };
+const search = (state = {}, action) => {
+  switch (action.type) {
+    case "SET_SEARCH_RES":
+      return action.payload;
+  }
+  return state;
+};
+
+function* fetchFavs() {
+  try {
+    const result = yield axios.get("/api/favorites");
+    yield put({ type: "REFRESH_FAVORITES", payload: result.data });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function* rootSaga() {
+  yield takeLatest("FETCH_FAV", fetchFavs);
+}
 
 const store = createStore(
   combineReducers({
     favorites,
     categories,
+    search,
   }),
   applyMiddleware(sagaMiddleware, logger)
 );
